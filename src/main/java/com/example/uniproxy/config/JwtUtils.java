8c990029ee5c05b6b5f3f2f286f44d6283687e63
vanuja2 <vanujaalagiyawanna@gmail.com>
2026-04-21 23:48:07 +0530

    create JwtUtils.java class

---
+++ src/main/java/com/example/uniproxy/config/JwtUtils.java
@@ -0,0 +1,38 @@
+package com.example.uniproxy.config;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+    private final String jwtSecret = "yourSecretKeyForUniProxyProject2026Secure"; // ලොකු secret key එකක් දෙන්න
+    private final int jwtExpirationMs = 86400000; // පැය 24ක් valid
+
+    private final Key key = Keys.hmacShaKeyFor(jwtSecret.getBytes());
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String getUsernameFromToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(key).build()
+                .parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
